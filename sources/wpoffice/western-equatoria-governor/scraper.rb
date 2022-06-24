667364722d86aca85bf75753eb9e02be6cb4f08d
Tony Bowden <tony@tmtm.com>
2022-06-24 12:59:42 +0300

    compare enwiki Administrators of Western Equatoria

---
+++ sources/wpoffice/western-equatoria-governor/scraper.rb
@@ -0,0 +1,32 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator WikidataIdsDecorator::Links
+
+  def holder_entries
+    noko.xpath("//h2[.//span[contains(.,'Governor')]]//following-sibling::ul[1]//li")
+  end
+
+  class Officeholder < OfficeholderNonTableBase
+    def name_node
+      noko.css('a').first
+    end
+
+    def raw_combo_date
+      years = noko.text.split(':').first
+      years =~ /^\d{4}$/ ? "#{years} - #{years}" : years
+    end
+
+    def empty?
+      name_node.nil? || noko.text.include?("broken up") || too_early?
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
