18f2d7ab80a96b2fa9ceb19b45bc8bda7cca3bfb
Tony Bowden <tony@tmtm.com>
2022-06-25 20:23:33 +0300

    refresh 2011 Caretaker Cabinet

--- sources/cabinet/2011-1/add-p39.js
+++ sources/cabinet/2011-1/add-p39.js
@@ -4,9 +4,9 @@ let meta = JSON.parse(rawmeta);
 
 module.exports = (id, label, position, positionlabel, startdate, enddate) => {
   qualifier = { }
-  if(startdate)              qualifier['P580']   = startdate
-  if(enddate)                qualifier['P582']   = enddate
-  if(!startdate && !enddate) qualifier['P5054]'] = meta.cabinet.it
+  if(startdate)              qualifier['P580']  = startdate
+  if(enddate)                qualifier['P582']  = enddate
+  if(!startdate && !enddate) qualifier['P5054'] = meta.cabinet.id
 
   return {
     id,
